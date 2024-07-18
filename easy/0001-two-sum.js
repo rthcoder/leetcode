ebb4f43b2d410916f28367db91e39c79fc685ec7
Tohir <tohirdavrsmart@gmail.com>
2024-07-18 10:12:56 +0500

    solve problem 0001-two-sum

--- easy/0001-two-sum.js
+++ easy/0001-two-sum.js
@@ -4,5 +4,22 @@
  * @return {number[]}
  */
 var twoSum = function (nums, target) {
+    const result_arr = []
 
+    for (let i = 0; i <= nums.length; i++) {
+        const new_target = target - nums[i]
+
+        for (let j = i + 1; j <= nums.length; j++) {
+
+            const num = nums[j]
+
+            if (num === new_target) {
+                return [i, j]
+            }
+
+        }
+    }
+    return false
 };
+
+console.log(twoSum([2, 7, 11, 15], 17));
