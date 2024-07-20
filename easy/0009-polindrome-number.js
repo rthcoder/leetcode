9bb2d427d33acdde6058f33e39c3c09bd43ae5e2
Tohir <tohirdavrsmart@gmail.com>
2024-07-20 19:52:40 +0500

    solve problem 0009-polindrome-number

---
+++ easy/0009-polindrome-number.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+var isPalindrome = function (x) {
+    if (x < 0) return false
+
+    let a = x.toString().split("").reverse().join("")
+
+    return parseInt(a) === x
+};
+
+console.log(isPalindrome(0));
