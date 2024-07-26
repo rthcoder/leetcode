fd69e6668eb2fefc3d75af43e189acb3567471ad
Tohir <tohirdavrsmart@gmail.com>
2024-07-26 18:28:21 +0500

    solve problem 0021-merge-two-sorted-list

---
+++ easy/0021-merge-two-sorted-list.js
@@ -0,0 +1,22 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} list1
+ * @param {ListNode} list2
+ * @return {ListNode}
+ */
+var mergeTwoLists = function (list1, list2) {
+    const arr = []
+
+    arr.push(list1.sort())
+    arr.push(list2.sort())
+
+    return arr.flat()
+};
+
+//leetcode qabul qilinmadi
