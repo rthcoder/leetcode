b8dd511ccbdfcf82f3dd3304a1a951a032c3ee92
Tohir <tohirdavrsmart@gmail.com>
2024-08-02 14:46:05 +0500

    0035-search-insert-position

---
+++ easy/0035-search-insert-position.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var searchInsert = function (nums, target) {
+    let left = 0;
+    let right = nums.length;
+
+    while (left < right) {
+        const middle = Math.floor((left + right) / 2);
+
+        if (nums[middle] < target) {
+            left = middle + 1;
+        } else {
+            right = middle;
+        }
+    }
+
+    return left;
+};
+console.log(searchInsert([1, 3, 5, 6], 2));
