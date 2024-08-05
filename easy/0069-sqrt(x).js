c41562f92319a638e40798a5b1084707696f1188
Tohir <tohirdavrsmart@gmail.com>
2024-08-05 10:59:33 +0500

    solve problem 0069-sqrt(x)

---
+++ easy/0069-sqrt(x).js
@@ -0,0 +1,25 @@
+/**
+ * @param {number} x
+ * @return {number}
+ */
+var mySqrt = function (x) {
+    let begin = 0;
+    let end = x
+    let result = 0;
+
+    while (begin <= end) {
+        let middle = Math.floor((begin + end) / 2);
+        console.log(middle, 'middle');
+
+        if (middle * middle > x) {
+            end = middle - 1;
+        }
+
+        else {
+            result = middle;
+            begin = middle + 1;
+        }
+    }
+    return result + 'result';
+};
+console.log(mySqrt(4));
